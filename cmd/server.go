e4867a5096d4211ca4937bd28536205998106be0
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-09-08 03:10:27 +0800

    feat: Integration work to invoke app from server command

--- cmd/server.go
+++ cmd/server.go
@@ -1,17 +1,44 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/hairizuanbinnoorazman/techmeetup/app"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"gopkg.in/fsnotify.v1"
+)
 
 var (
 	serverCmd = func() *cobra.Command {
+		var configFile string
 		cmd := &cobra.Command{
 			Use:   "server",
-			Short: "server will start a bunch of operations to handle workflows in mgmt of tech meetups",
+			Short: "Server will start a bunch of operations to handle workflows in mgmt of tech meetups",
 			Long:  ``,
 			Run: func(cmd *cobra.Command, args []string) {
-				cmd.Help()
+				notifyConfigChange := make(chan bool)
+				notifyInterrupts := make(chan os.Signal, 1)
+				signal.Notify(notifyInterrupts, syscall.SIGINT, syscall.SIGTERM)
+
+				watcher, err := fsnotify.NewWatcher()
+				if err != nil {
+					logrus.Error("Unable to start file watch")
+				}
+				defer watcher.Close()
+				watcher.Add(configFile)
+				go app.ConfigFileWatcher(watcher, notifyConfigChange)
+
+				configStore := app.NewBasicConfigStore(configFile)
+				runner := app.NewApp(configStore, logrus.New())
+				runner.Initialize()
+				runner.Run(notifyConfigChange, notifyInterrupts)
 			},
 		}
+		cmd.Flags().StringVar(&configFile, "config", "config.yaml", "Configuration file. Please utilize the fetcher to ensure the right format of config is used")
 		return cmd
 	}
 )
