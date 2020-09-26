0098b4bb2e217f77ad8eb64e272bc86c04a113c7
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-09-26 17:32:41 +0800

    new possible application configuration

--- app/config.go
+++ app/config.go
@@ -34,19 +34,41 @@ func (b BasicConfigStore) Get() (Config, error) {
 }
 
 type Config struct {
-	Authstore string            `yaml:"authstore"`
-	Features  Features          `yaml:"features"`
-	Meetup    MeetupCredentials `yaml:"meetup_credentials"`
-	Google    GoogleCredentials `yaml:"google_credentials"`
+	Authstore        string                `yaml:"authstore"`
+	EventStoreFile   string                `yaml:"eventstore"`
+	Features         Features              `yaml:"features"`
+	Meetup           MeetupCredentials     `yaml:"meetup_credentials"`
+	Google           GoogleCredentials     `yaml:"google_credentials"`
+	Streamyard       StreamyardCredentials `yaml:"streamyard_credentials"`
+	SpreadsheetStats string                `yaml:"spreadsheet_stats"`
+	CalendarConfig   CalendarConfig        `yaml:"calendar_config"`
+	MeetupConfig     MeetupConfig          `yaml:"meetup_config"`
+	StreamyardConfig StreamyardConfig      `yaml:"streamyard_config"`
 }
 
 type Features struct {
-	MeetupSync FeatureControl `yaml:"meetup_sync"`
+	MeetupSync  MeetupFeatureControl `yaml:"meetup_sync"`
+	AuthRefresh FeatureControl       `yaml:"auth_refresh"`
 }
 
 type FeatureControl struct {
 	Enabled      bool `yaml:"enabled"`
 	IdleDuration int  `yaml:"idle_duration"`
+}
+
+type MeetupFeatureControl struct {
+	Enabled      bool                    `yaml:"enabled"`
+	IdleDuration int                     `yaml:"idle_duration"`
+	SubFeatures  SubMeetupFeatureControl `yaml:"subfeatures"`
+}
+
+type SubMeetupFeatureControl struct {
+	StreamyardSync     bool `yaml:"streamyard_sync"`
+	CalendarSync       bool `yaml:"calendar_sync"`
+	MeetupSync         bool `yaml:"meetup_sync"`
+	SlidesSync         bool `yaml:"slides_sync"`
+	SheetsReporterSync bool `yaml:"sheets_reporter_sync"`
+	PostYoutubeSync    bool `yaml:"post_youtube_sync"`
 }
 
 type MeetupCredentials struct {
@@ -60,4 +82,25 @@ type GoogleCredentials struct {
 	ClientSecret string `yaml:"client_secret"`
 	Scope        string `yaml:"scope"`
 	RedirectURI  string `yaml:"redirect_uri"`
+}
+
+type StreamyardCredentials struct {
+	CSRFToken string `yaml:"csrf_token"`
+	JWT       string `yaml:"jwt"`
+}
+
+type CalendarConfig struct {
+	CalendarID              string `yaml:"calendar_id"`
+	CalendarEventInvitation string `yaml:"calendar_event_invitation"`
+}
+
+type MeetupConfig struct {
+	MeetupGroup      string            `yaml:"meetup_group"`
+	OrganizerMapping map[string]string `yaml:"organizer_mapping"`
+}
+
+type StreamyardConfig struct {
+	UserID                   string `yaml:"user_id"`
+	YoutubeDestination       string `yaml:"youtube_destination"`
+	FacebookGroupDestination string `yaml:"facebook_group_destination"`
 }
