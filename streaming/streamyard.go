e2e594aa32aa0e06418ff62f26e500193f13df7a
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-09-30 21:40:26 +0800

    add checks before running of update streamyard operations

--- streaming/streamyard.go
+++ streaming/streamyard.go
@@ -21,13 +21,13 @@ import (
 
 type Stream struct {
 	ID           string
-	StartDate    time.Time
-	Name         string
-	Description  string
-	ImagePath    string
-	YoutubeLink  string
-	IsPublic     bool
 	Destinations []Destination
+	// Inputs
+	StartDate   time.Time
+	Name        string
+	Description string
+	ImagePath   string
+	IsPublic    bool
 }
 
 type Destination struct {
@@ -57,6 +57,10 @@ type StreamyardBroadcastResponse struct {
 	Status  string                              `json:"status"`
 	Title   string                              `json:"title"`
 	Outputs []StreamyardBroadcastOutputResponse `json:"outputs"`
+}
+
+type StreamyardDestinationResponse struct {
+	Output StreamyardBroadcastOutputResponse `json:"output"`
 }
 
 type StreamyardBroadcastOutputResponse struct {
@@ -191,19 +195,28 @@ func (s Streamyard) CreateStream(ctx context.Context, title string) (Stream, err
 	}
 	raw, err := ioutil.ReadAll(resp.Body)
 
-	s.logger.Info(string(raw))
+	var aa StreamyardBroadcastResponse
+	err = json.Unmarshal(raw, &aa)
+	if err != nil {
+		return Stream{}, fmt.Errorf("Unable to retrieve parse response from streamyard. Err: %v", err)
+	}
 
-	return Stream{}, nil
+	// s.logger.Info(string(raw))
+
+	return Stream{
+		ID:   aa.ID,
+		Name: aa.Title,
+	}, nil
 }
 
 func (s Streamyard) CreateDestination(ctx context.Context, destinationStreamType string, ss Stream) (Stream, error) {
-	if destinationStreamType == "" || ss.ID == "" {
+	if destinationStreamType == "" || ss.ID == "" || ss.Name == "" || ss.Description == "" || ss.ImagePath == "" || ss.StartDate.IsZero() {
 		return Stream{}, fmt.Errorf("Destination Stream Type is empty or no reference pass for which stream to adjust. Do relook inputs")
 	}
 
 	err := s.jwtChecker()
 	if err != nil {
-		return Stream{}, fmt.Errorf("Error while checking jwt. Err: %v", err)
+		return ss, fmt.Errorf("Error while checking jwt. Err: %v", err)
 	}
 
 	initialURL := fmt.Sprintf("https://streamyard.com/api/broadcasts/%v/outputs", ss.ID)
@@ -214,12 +227,12 @@ func (s Streamyard) CreateDestination(ctx context.Context, destinationStreamType
 
 	rawImage, err := ioutil.ReadFile(ss.ImagePath)
 	if err != nil {
-		return Stream{}, fmt.Errorf("Unable to load image file. Please check path to ensure correct. Err: %v", err)
+		return ss, fmt.Errorf("Unable to load image file. Please check path to ensure correct. Err: %v", err)
 	}
 
 	imageContentType, err := imageTypeDetector(ss.ImagePath)
 	if err != nil {
-		return Stream{}, fmt.Errorf("Unexpected Image Type found. Please review file type. Err: %v", err)
+		return ss, fmt.Errorf("Unexpected Image Type found. Please review file type. Err: %v", err)
 	}
 
 	body := new(bytes.Buffer)
@@ -228,12 +241,17 @@ func (s Streamyard) CreateDestination(ctx context.Context, destinationStreamType
 	part.Write([]byte(ss.Name))
 	part, _ = writer.CreateFormField("description")
 	part.Write([]byte(ss.Description))
-	if ss.IsPublic {
-		part, _ = writer.CreateFormField("privacy")
-		part.Write([]byte("public"))
+	if destinationStreamType == "youtube" {
+		if ss.IsPublic {
+			part, _ = writer.CreateFormField("privacy")
+			part.Write([]byte("public"))
+		} else {
+			part, _ = writer.CreateFormField("privacy")
+			part.Write([]byte("private"))
+		}
 	} else {
 		part, _ = writer.CreateFormField("privacy")
-		part.Write([]byte("private"))
+		part.Write([]byte("public"))
 	}
 
 	// Special code to cope with custom type
@@ -255,7 +273,7 @@ func (s Streamyard) CreateDestination(ctx context.Context, destinationStreamType
 		part, _ = writer.CreateFormField("destinationId")
 		part.Write([]byte(s.facebookDestination))
 	} else {
-		return Stream{}, fmt.Errorf("Bad destination location")
+		return ss, fmt.Errorf("Bad destination location")
 	}
 
 	part, _ = writer.CreateFormField("csrfToken")
@@ -270,15 +288,29 @@ func (s Streamyard) CreateDestination(ctx context.Context, destinationStreamType
 	resp, err := s.client.Do(req)
 	raw, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return Stream{}, err
+		return ss, err
 	}
-	s.logger.Info(string(raw))
-	return Stream{}, nil
+
+	// s.logger.Info(string(raw))
+
+	var aa StreamyardDestinationResponse
+	err = json.Unmarshal(raw, &aa)
+	if err != nil {
+		return ss, fmt.Errorf("Unable to parse streamyard destination response")
+	}
+
+	ss.Destinations = append(ss.Destinations, Destination{
+		ID:   aa.Output.ID,
+		Type: aa.Output.Platform,
+		Link: aa.Output.PlatformLink,
+	})
+
+	return ss, nil
 }
 
 func (s Streamyard) UpdateDestination(ctx context.Context, ss Stream, destinationID string) (Stream, error) {
 	if ss.ID == "" || destinationID == "" {
-		return Stream{}, fmt.Errorf("No stream ID reference or destination ID reference provided. Please recheck inputs")
+		return ss, fmt.Errorf("No stream ID reference or destination ID reference provided. Please recheck inputs")
 	}
 
 	err := s.jwtChecker()
@@ -294,12 +326,12 @@ func (s Streamyard) UpdateDestination(ctx context.Context, ss Stream, destinatio
 
 	rawImage, err := ioutil.ReadFile(ss.ImagePath)
 	if err != nil {
-		return Stream{}, fmt.Errorf("Unable to load image file. Please check path to ensure correct. Err: %v", err)
+		return ss, fmt.Errorf("Unable to load image file. Please check path to ensure correct. Err: %v", err)
 	}
 
 	imageContentType, err := imageTypeDetector(ss.ImagePath)
 	if err != nil {
-		return Stream{}, fmt.Errorf("Unexpected Image Type found. Please review file type. Err: %v", err)
+		return ss, fmt.Errorf("Unexpected Image Type found. Please review file type. Err: %v", err)
 	}
 
 	body := new(bytes.Buffer)
@@ -333,10 +365,18 @@ func (s Streamyard) UpdateDestination(ctx context.Context, ss Stream, destinatio
 	resp, err := s.client.Do(req)
 	raw, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return Stream{}, err
+		return ss, err
 	}
+
 	s.logger.Info(string(raw))
-	return Stream{}, nil
+
+	var aa StreamyardBroadcastOutputResponse
+	err = json.Unmarshal(raw, &aa)
+	if err != nil {
+		return ss, fmt.Errorf("Unable to parse json response from Streamyard. Err: %v", err)
+	}
+
+	return ss, nil
 }
 
 type StreamyardDestinationResp struct {
